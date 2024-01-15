d985de132f2e28561d1520fdaff00920ba2615ed
luqmanzakariya <luqman.zakariya@gmail.com>
2024-01-15 18:57:04 +0700

    feature/member-overview: create component card member overview

--- src/components/organisms/dashboard/member-overview-card/index.tsx
+++ src/components/organisms/dashboard/member-overview-card/index.tsx
@@ -1,12 +1,14 @@
 "use client"
+import React from "react";
 
 import Card from "@/components/atoms/card";
-import MoneyText from "@/components/atoms/text/money";
+// import MoneyText from "@/components/atoms/text/money";
 import TitleText from "@/components/atoms/text/title";
 import SubtitleText from "@/components/atoms/text/subtitle";
-import Icon from "@/components/atoms/icon";
+// import Icon from "@/components/atoms/icon";
+import CardMemberOverview from "@/components/atoms/card/member-overview";
 
-const Header = ({ title }) => {
+const Header = () => {
 	return (
 		<div className="card_header">
 			<TitleText>
@@ -19,12 +21,53 @@ const Header = ({ title }) => {
 	)
 }
 
-const MemberOverviewCard = () => {
+const listMemberOverview = [
+	{
+		title: "Total Member",
+		value: "54,695",
+		comparison: "23.8%",
+		compareLastYear: 24
+	},
+	{
+		title: "New Member",
+		value: "6,458",
+		comparison: "-16.5%",
+		compareLastYear: -8
+	},
+	{
+		title: "Active Member",
+		value: "24,396",
+		comparison: "23.8%",
+		compareLastYear: 24
+	},
+	{
+		title: "Total Deposit",
+		value: "500.950.450,00",
+		comparison: "-16.5%",
+		compareLastYear: -8
+	},
+	{
+		title: "New Deposit",
+		value: "37.695.800,00",
+		comparison: "23.8%",
+		compareLastYear: 24
+	},
+	{
+		title: "Total Withdraw",
+		value: "37.695.800,00",
+		comparison: "-16.5%",
+		compareLastYear: -8
+	}
+]
+
+const MemberOverviewCard: React.FC = () => {
 	return (
 		<Card
 			headerElement={<Header />}
 		>
-			Member Overview
+			<div className="@apply grid grid-cols-[auto_auto_auto] gap-x-5 gap-y-5 p-5">
+				{listMemberOverview.map((values) => <CardMemberOverview {...values} />)}
+			</div>
 		</Card> 
 	)
 }
