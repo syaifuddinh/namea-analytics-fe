872fa1ceadaabae40be9de0823e1d4777db279c1
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2024-01-29 13:00:50 +0700

    [EDIT] Remove letter spacing

--- src/components/atoms/text/title/index.tsx
+++ src/components/atoms/text/title/index.tsx
@@ -2,7 +2,7 @@ import "./index.style.css"
 
 const TitleText = ({ children }) => {
 	return (
-		<div className="title-text font-base text-lg -tracking-[0.216px] leading-[26px]">	
+		<div className="title-text font-base text-lg leading-[26px]">	
 			{ children }
 		</div>	
 	)
--- src/components/organisms/dashboard/monthly-insight-card/index.tsx
+++ src/components/organisms/dashboard/monthly-insight-card/index.tsx
@@ -69,7 +69,7 @@ const Header = ({
 								Profit Margin
 							</div>
 						</div>
-						<div className="text-white-theme-color mt-1.5 font-base flex items-center gap-2.5 -tracking-[0.16px]">
+						<div className="text-white-theme-color mt-1.5 font-light flex items-center gap-2.5 -tracking-[0.16px]">
 							<Pile variant="primary" />
 							{ profitMargin }%
 						</div>
