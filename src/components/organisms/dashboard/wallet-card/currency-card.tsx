9719aff50171212533dba3d57b6bd939ed47f1f7
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2024-02-01 15:49:11 +0700

    [EDIT] Turn over : adjust height

--- src/components/organisms/dashboard/wallet-card/currency-card.tsx
+++ src/components/organisms/dashboard/wallet-card/currency-card.tsx
@@ -15,7 +15,7 @@ const CurrencyCard = ({
 }) => {
 	return (
 		<SecondaryCard
-			className="bg-base-bg5 max-h-[11.5rem]"
+			className="bg-base-bg5 max-h-[11.5rem] min-h-[11.5rem] flex flex-col justify-between"
 		>	
 			<div className="p-[1.125rem] pb-2.5">	
 				<div className="text-sm text-gray6 mb-1 font-light -tracking-[0.14px] leading-5">	
