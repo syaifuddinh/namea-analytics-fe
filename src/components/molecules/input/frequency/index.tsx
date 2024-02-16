68f5290afcfd56f3b2c5792a64555670a8d3eb78
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2024-02-16 15:12:33 +0700

    [REFACTOR] Create independent component for row option input

--- src/components/molecules/input/frequency/index.tsx
+++ src/components/molecules/input/frequency/index.tsx
@@ -2,6 +2,7 @@
 
 import { useState } from "react"
 import { useEffect } from "react"
+import RowOptionInput from "@/components/molecules/input/row-option"
 
 const frequencies = [
 	{
@@ -27,48 +28,14 @@ export default function FrequencyInput({
 	value,
 	onChange
 }: FrequencyInputType) {
-	const [datasets, setDatasets] = useState(frequencies)
-	const [dataValue, setDataValue] = useState(() => {
-		if(!value) return frequencies[0].value;
-		return value
-	})
-
-	const onChangeData = newValue => {
-		setDataValue(newValue)
-		onChange(newValue)
-	}
-
-	const onAssignValue = () => {
-		if(!value) return;
-		if(value === dataValue) return
-		setDataValue(value)
-	}
-
-	useEffect(() => {
-		onAssignValue()
-
-		return () => false
-	}, [value])
 
  	return (
-	    <div className="frequency-input border border-[var(--Gray4)] rounded-[0.625rem] p-[0.21rem] flex gap-1 -tracking-[0.14px] min-h-[36px] max-h-[36px] min-w-[216px] max-w-[216px]">
-	    	{ 
-	    		datasets.map(item => (
-			    	<div
-			    		key={item.value}
-			    		className={`frequency-input_item max-h-[1.75rem] py-[4px] px-[10px] cursor-pointer ${item.value === dataValue ? "text-[var(--Gray10)] bg-[var(--base-bg3)] rounded-[6px] border border-[var(--Gray1)]" : "text-[var(--Gray6)]"}`}
-			    		onClick={() => onChangeData(item.value)}
-			    	>
-
-			    		<div
-			    			className="font-extralight tracking-[0.06px] text-sm leading-5"
-			    		>
-			    			
-				    		{ item.label }
-			    		</div>
-			    	</div>
-	    		))
-	    	}
+	    <div>
+	    	<RowOptionInput
+	    		value={value}
+	    		onChange={onChange}
+	    		options={frequencies}
+	    	/>
 	    </div>
  	)
 }
