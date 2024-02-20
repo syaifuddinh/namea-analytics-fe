f0d6c4f83c469c3ae21f80611e00539010d4be1c
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2024-02-20 14:18:47 +0700

    [EDIT] Cashflow overview : change letter spacing

--- src/app/cashflow/page.tsx
+++ src/app/cashflow/page.tsx
@@ -12,8 +12,8 @@ export default function CashflowPage() {
     <>
       <Header className="mt-[2.775rem]" />
       <UploadCSV className="mt-[1.5rem]" />
-      <Divider className="mt-6" />
-      <CashflowOverviewCard className="mt-[1.5rem]" />
+      <Divider className="mt-4" />
+      <CashflowOverviewCard className="mt-8" />
       <Comparison className="mt-[1.5rem]" />
       <FinancialStatement className="mt-[1.5rem]" />
     </>
