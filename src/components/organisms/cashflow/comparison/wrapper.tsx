efb7c36beaeeeafac3a3c2472f0c6b279166d169
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2024-02-20 17:30:29 +0700

    [EDIT] Cashflow / Comparison : change padding

--- src/components/organisms/cashflow/comparison/wrapper.tsx
+++ src/components/organisms/cashflow/comparison/wrapper.tsx
@@ -2,7 +2,7 @@ import "./wrapper.style.css"
 
 export default function Wrapper({ children, className }) {
 	return (
-		<div className={`comparison-wrapper p-[1.125rem] border border-[var(--base-bg4)] flex flex-col lg:flex-row items-center justify-between rounded-[12px] *:w-full gap-3 ${className}`}>
+		<div className={`comparison-wrapper p-[1.25rem] border border-[var(--base-bg4)] flex flex-col lg:flex-row items-center justify-between rounded-[12px] *:w-full gap-3 ${className}`}>
 			{ children }
 		</div>
 	)
