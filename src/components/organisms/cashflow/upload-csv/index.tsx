80a5d9ff36f5bef6509a2b5a858df21d73f8e416
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2024-02-20 12:26:55 +0700

    [EDIT] Cashflow / Upload CSV : adjust gap

--- src/components/organisms/cashflow/upload-csv/index.tsx
+++ src/components/organisms/cashflow/upload-csv/index.tsx
@@ -10,16 +10,16 @@ export default function UploadCSV({
 			className={"upload-csv lg:flex-row flex-col flex lg:items-center justify-between gap-4 rounded-[16px] p-5 " + className}
 		>
 			<div className="upload-csv_entry">
-				<div className="upload-csv_title font-light text-[18px] text-[var(--Gray10)] leading-[26px]">
+				<div className="upload-csv_title font-light text-normal text-[var(--Gray10)] leading-[26px]">
 					Upload CSV
 				</div>
-				<div className="upload-csv_description font-extralight text-sm leading-5 text-[var(--Gray6)]">
+				<div className="upload-csv_description mt-1 font-extralight text-sm leading-5 text-[var(--Gray6)]">
 					Syncs all your cashflow data by uploading your CSV file here.
 				</div>
 			</div>
 
 			<div className="upload-csv_hall flex flex-col sm:flex-row items-start sm:items-center">
-				<div className="upload-csv_last-update font-extralight text-sm leading-5 text-[var(--Gray6)] mr-5">
+				<div className="upload-csv_last-update font-extralight text-sm leading-5 text-[var(--Gray6)] mr-[1.5rem]">
 					Last uploaded: 18 January 2023, 2:30PM
 				</div>
 
