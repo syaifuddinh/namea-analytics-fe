6fd3f7df2d94489987f027ffba3037f543af1e31
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2024-02-22 16:11:44 +0700

    [ADD] Costing : add submenu

---
+++ src/app/costing/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import Submenu from "@/components/organisms/costing/submenu"
+import Divider from "@/components/atoms/divider"
+
+export default function CashflowPage() {
+  return (
+    <>
+      <Submenu />
+      <Divider className="absolute left-0" />
+    </>
+  );
+}
