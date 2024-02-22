ba689777bb5ab084802c88334899fc9785637299
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2024-02-22 16:34:06 +0700

    [ADD] Costing : add upload csv section

--- src/app/costing/page.tsx
+++ src/app/costing/page.tsx
@@ -2,6 +2,7 @@
 
 import Submenu from "@/components/organisms/costing/submenu"
 import Header from "@/components/organisms/costing/header"
+import UploadCSV from "@/components/organisms/costing/upload-csv"
 import Divider from "@/components/atoms/divider"
 
 export default function CashflowPage() {
@@ -10,6 +11,7 @@ export default function CashflowPage() {
       <Submenu />
       <Divider className="absolute left-0" />
       <Header className="mt-8" />
+      <UploadCSV className="mt-[1.5rem]" />
     </>
   );
 }
