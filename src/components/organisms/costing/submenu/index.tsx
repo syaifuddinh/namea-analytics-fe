e48315380989394d3075d3da75e443a84455b2be
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2024-02-27 12:05:54 +0700

    [EDIT] Costing / Submenu : adjust padding

--- src/components/organisms/costing/submenu/index.tsx
+++ src/components/organisms/costing/submenu/index.tsx
@@ -24,7 +24,7 @@ export default function Submenu() {
 					key={item.id}
 					className={`flex gap-[0.625rem] justify-center items-center rounded-[21.321px] border bg-base-bg3 border-base-bg3 text-gray-6 text-sm font-extralight max-h-[2rem] min-h-[2rem] cursor-pointer ${!item.description ? "px-3" : " pl-[3px] pr-3"}`}
 				>
-					<div className={(item.description ? "px-3 bg-base-bg3 h-[24px] flex items-center rounded-[21.312px] text-gray-10" : "")}>
+					<div className={(item.description ? "px-[0.625rem] bg-base-bg3 h-[24px] flex items-center rounded-[21.312px] text-gray-10" : "")}>
 						{ item.name }	
 					</div>
 
