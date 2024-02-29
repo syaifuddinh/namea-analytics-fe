cd24d466e3f29472af5531357c62a7137c75f13f
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2024-02-29 16:00:21 +0700

    [ADD] Costing / Promotional : add monthly expense component

--- src/app/costing/promotional/page.tsx
+++ src/app/costing/promotional/page.tsx
@@ -4,6 +4,7 @@ import UploadCSV from "@/components/organisms/costing/upload-csv"
 import CostingBreakdown from "@/components/organisms/costing/breakdown"
 import GeneralCosting from "@/components/organisms/costing/general-costing"
 import Comparison from "@/components/organisms/costing/comparison"
+import MonthlyExpense from "@/components/organisms/costing/monthly-expense"
 import Header from "@/components/organisms/costing/header"
 import TrendAnalysis from "@/components/organisms/costing/trend-analysis"
 import PromotionalCostingTab from "@/components/organisms/costing/promotional-costing-tab"
@@ -27,6 +28,7 @@ export default function PromotionalCostingPage() {
             }
     	/>
         <TrendAnalysis className="mt-[3.25rem]" />
+        <MonthlyExpense className="mt-[3.25rem]" />
     </CostingLayout>
   );
 }
---
+++ src/components/organisms/costing/monthly-expense/index.tsx
@@ -0,0 +1,7 @@
+export default function MonthlyExpense({}) {
+	return (
+		<div>
+			
+		</div>
+	)
+}
