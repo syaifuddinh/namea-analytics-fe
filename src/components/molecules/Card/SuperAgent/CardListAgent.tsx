081e55a2c859fc8195cc3c9ffe5c357a697b99f7
jordyandreas <jordyandreas76@gmail.com>
2024-03-05 19:20:08 +0700

    fix super agent detail 4 - [x] Text Della J. Mitchell = width :158px - [x] - Jarak Text Profit Bitcoin, Ethereum, Turn Over, Total Profit ke nilai di bawahnya = 4px - [x] - Nilai Profit Bitcoin, Ethereum, Turn Over, Total Profit = font weight :300 - [x] Area Profit Bitcoin, Profit Etherium, Total Turnover, Total Profit= padding L :46px - [x] - Text "Profit Bitcoin, Ethereum, Turn Over, Total Profit" = font weight :200 - [x] - Text "Della J. Mitchel" = font weight :300 - [x] - Text "JMS88" = font weight :300 - [x] - Overlay di Atas "Della J. Mitchel"  di hapus

--- src/components/molecules/Card/SuperAgent/CardListAgent.tsx
+++ src/components/molecules/Card/SuperAgent/CardListAgent.tsx
@@ -16,120 +16,125 @@ export const CardListAgent: React.FC<ICardListAgent> = ({
 }) => {
   return (
     <div className="flex flex-1 border border-gray-1 rounded-2xl bg-base-bg2 overflow-hidden mb-4">
-      <div className="flex flex-1 flex-col border-t border-gray-1 mt-[6px] bg-base-bg5 rounded-t-xl">
-        <div className="flex flex-1 flex-row px-5 pb-5 divide-x divide-gray-1 items-center pt-5 ">
-          <div className="flex flex-row items-center">
-            <Image src={image} alt="super agent icon" width={40} height={40} />
-            <div className="text-gray-10 text-xs py-1 px-2 mx-3 bg-base-bg3 rounded-3xl">
-              {codeAgent}
+      <div className="flex flex-1 flex-row px-5 pb-5 divide-x divide-gray-1 items-center pt-5 ">
+        <div className="flex flex-row items-center">
+          <Image src={image} alt="super agent icon" width={40} height={40} />
+          <div className="text-gray-10 text-xs py-1 px-2 mx-3 bg-base-bg3 rounded-3xl font-light">
+            {codeAgent}
+          </div>
+          <div className="w-[158px] text-gray-10 text-sm mr-[0.125rem] font-light">
+            {nameAgent}
+          </div>
+        </div>
+        <div className="flex flex-1 flex-row items-center pl-[46px]">
+          <div className="flex flex-1 flex-col">
+            <div className="text-gray-6 text-sm font-extralight mb-1">
+              Profit Bitcoin
             </div>
-            <div className="w-[160px] text-gray-10 text-sm mr-[0.125rem]">
-              {nameAgent}
+            <div
+              className={clsx(
+                "text-sm font-light",
+                bitcoin.profit ? "text-gray-10" : "text-alert-10",
+                bitcoin.nominal.includes("Rp") && "flex flex-row items-center"
+              )}
+            >
+              {bitcoin.nominal}
+              {bitcoin.nominal.includes("Rp") && (
+                <div
+                  className={bitcoin.profit ? "text-gray-6" : "text-alert-6"}
+                >
+                  ,00
+                </div>
+              )}
             </div>
           </div>
-          <div className="flex flex-1 flex-row items-center pl-12">
-            <div className="flex flex-1 flex-col">
-              <div className="text-gray-6 text-sm">Profit Bitcoin</div>
-              <div
-                className={clsx(
-                  "text-sm",
-                  bitcoin.profit ? "text-gray-10" : "text-alert-10",
-                  bitcoin.nominal.includes("Rp") && "flex flex-row items-center"
-                )}
-              >
-                {bitcoin.nominal}
-                {bitcoin.nominal.includes("Rp") && (
-                  <div
-                    className={bitcoin.profit ? "text-gray-6" : "text-alert-6"}
-                  >
-                    ,00
-                  </div>
-                )}
-              </div>
+          <div className="flex flex-1 flex-col">
+            <div className="text-gray-6 text-sm font-extralight mb-1">
+              Profit Ethereum
             </div>
-            <div className="flex flex-1 flex-col">
-              <div className="text-gray-6 text-sm">Profit Ethereum</div>
-              <div
-                className={clsx(
-                  "text-sm",
-                  ethereum.profit ? "text-gray-10" : "text-alert-10",
-                  ethereum.nominal.includes("Rp") &&
-                    "flex flex-row items-center"
-                )}
-              >
-                {ethereum.nominal}
-                {ethereum.nominal.includes("Rp") && (
-                  <div
-                    className={ethereum.profit ? "text-gray-6" : "text-alert-6"}
-                  >
-                    ,00
-                  </div>
-                )}
-              </div>
+            <div
+              className={clsx(
+                "text-sm font-light",
+                ethereum.profit ? "text-gray-10" : "text-alert-10",
+                ethereum.nominal.includes("Rp") && "flex flex-row items-center"
+              )}
+            >
+              {ethereum.nominal}
+              {ethereum.nominal.includes("Rp") && (
+                <div
+                  className={ethereum.profit ? "text-gray-6" : "text-alert-6"}
+                >
+                  ,00
+                </div>
+              )}
             </div>
-            <div className="flex flex-1 flex-col">
-              <div className="text-gray-6 text-sm">Total Turn Over</div>
-              <div
-                className={clsx(
-                  "text-sm",
-                  totalTurnOver.profit ? "text-gray-10" : "text-alert-10",
-                  totalTurnOver.nominal.includes("Rp") &&
-                    "flex flex-row items-center"
-                )}
-              >
-                {totalTurnOver.nominal}
-                {totalTurnOver.nominal.includes("Rp") && (
-                  <div
-                    className={
-                      totalTurnOver.profit ? "text-gray-6" : "text-alert-6"
-                    }
-                  >
-                    ,00
-                  </div>
-                )}
-              </div>
-            </div>
-            <div className="flex flex-1 flex-col">
-              <div className="text-gray-6 text-sm">Total Profit</div>
-              <div
-                className={clsx(
-                  "text-sm",
-                  totalProfit.profit ? "text-gray-10" : "text-alert-10",
-                  totalProfit.nominal.includes("Rp") &&
-                    "flex flex-row items-center"
-                )}
-              >
-                {totalProfit.nominal}
-                {totalProfit.nominal.includes("Rp") && (
-                  <div
-                    className={
-                      totalProfit.profit ? "text-gray-6" : "text-alert-6"
-                    }
-                  >
-                    ,00
-                  </div>
-                )}
-              </div>
-            </div>
-            <button className="flex flex-1 flex-row rounded-md border border-gray-4 bg-checkbox items-center px-[10px]">
-              <div
-                className={clsx(
-                  "h-2 w-2 rounded-full",
-                  status === "Active"
-                    ? "bg-dot-blue"
-                    : status === "In Progress"
-                    ? "bg-warning"
-                    : "bg-danger-medium"
-                )}
-              />
-              <div className="text-left w-[97px] text-gray-0 text-sm py-[6px] pl-2 mr-5">
-                {status}
-              </div>
-              <div className="flex flex-1 justify-end">
-                <IconChevronDown />
-              </div>
-            </button>
           </div>
+          <div className="flex flex-1 flex-col">
+            <div className="text-gray-6 text-sm font-extralight mb-1">
+              Total Turn Over
+            </div>
+            <div
+              className={clsx(
+                "text-sm font-light",
+                totalTurnOver.profit ? "text-gray-10" : "text-alert-10",
+                totalTurnOver.nominal.includes("Rp") &&
+                  "flex flex-row items-center"
+              )}
+            >
+              {totalTurnOver.nominal}
+              {totalTurnOver.nominal.includes("Rp") && (
+                <div
+                  className={
+                    totalTurnOver.profit ? "text-gray-6" : "text-alert-6"
+                  }
+                >
+                  ,00
+                </div>
+              )}
+            </div>
+          </div>
+          <div className="flex flex-1 flex-col">
+            <div className="text-gray-6 text-sm font-extralight mb-1">
+              Total Profit
+            </div>
+            <div
+              className={clsx(
+                "text-sm font-light",
+                totalProfit.profit ? "text-gray-10" : "text-alert-10",
+                totalProfit.nominal.includes("Rp") &&
+                  "flex flex-row items-center"
+              )}
+            >
+              {totalProfit.nominal}
+              {totalProfit.nominal.includes("Rp") && (
+                <div
+                  className={
+                    totalProfit.profit ? "text-gray-6" : "text-alert-6"
+                  }
+                >
+                  ,00
+                </div>
+              )}
+            </div>
+          </div>
+          <button className="flex flex-1 flex-row rounded-md border border-gray-9 bg-checkbox items-center pl-[10px] pr-[2px] w-[147px] h-[28px]">
+            <div
+              className={clsx(
+                "h-2 w-2 rounded-full",
+                status === "Active"
+                  ? "bg-dot-blue"
+                  : status === "In Progress"
+                  ? "bg-warning"
+                  : "bg-danger-medium"
+              )}
+            />
+            <div className="text-left w-[97px] text-gray-0 text-sm py-[6px] pl-2 font-extralight">
+              {status}
+            </div>
+            <div className="flex flex-1 justify-end">
+              <IconChevronDown className="!w-[1.188rem] !h-[1.15rem]" />
+            </div>
+          </button>
         </div>
       </div>
     </div>
