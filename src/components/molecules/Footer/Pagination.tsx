676217aa7beece5dfef8303cbfe4a1170cd19bdf
jordyandreas <jordyandreas76@gmail.com>
2024-03-13 14:22:00 +0700

    Footer - Text "Item per page" = Font Weight :200 - Text "Jumlah perpage yang di tampilan" (angka 10) = Font Weight :300, font size 12 - Area "Jumlah perpage yang di tampilan"(angka 10) = Padding T B :2px, L :10px,  R: 4px
    - arrow di kiri Previous dan kanan Next = W :1.25rem, H :1.5rem
    - Previous & next = font weight :300
    - text 1 2 3 4 = Font Weight :300
    - tambah page 4 di samping kanan page 3
    
    - text "28 Agents in total" = font-weight :200

--- src/components/molecules/Footer/Pagination.tsx
+++ src/components/molecules/Footer/Pagination.tsx
@@ -14,6 +14,7 @@ export const Pagination: React.FC<IPagination> = ({
     { label: "1", active: true },
     { label: "2", active: false },
     { label: "3", active: false },
+    { label: "4", active: false },
   ];
   return (
     <div
@@ -23,24 +24,24 @@ export const Pagination: React.FC<IPagination> = ({
       )}
     >
       <div className="flex flex-row items-center gap-2">
-        <div className="text-gray-6 text-sm">Item per page</div>
-        <div className="flex flex-row items-center bg-base-bg3 rounded-[4px] pl-2 py-1">
-          <div className="text-gray-10 text-sm pr-[2px]">10</div>
+        <div className="text-gray-6 text-sm font-extralight">Item per page</div>
+        <div className="flex flex-row items-center bg-base-bg3 rounded-[4px] pl-[10px] pr-1 py-[2px]">
+          <div className="text-gray-10 text-xs font-light pr-[2px]">10</div>
           <button>
-            <IconSmallChevronDown />
+            <IconSmallChevronDown className="!w-[1.25rem] !h-[1.5rem]" />
           </button>
         </div>
       </div>
       <div className="flex flex-row items-center rounded-lg px-4 bg-base-bg5 border border-base-bg3 text-sm">
         <button className="flex flex-row items-center pr-5">
           <IconArrowLeft />
-          <div className="text-gray-6 pl-2">Previous</div>
+          <div className="text-gray-6 pl-2 font-light">Previous</div>
         </button>
         <div className="flex flex-row items-center">
           {x.map((item) => (
             <button
               key={item.label}
-              className={`px-[15px] py-[10px] ${
+              className={`px-[15px] py-[10px] font-light ${
                 item.active
                   ? "text-gray-10 border border-base-bg3 bg-base-bg3"
                   : "text-gray-6 border-r border-r-base-bg3"
@@ -52,11 +53,13 @@ export const Pagination: React.FC<IPagination> = ({
         </div>
 
         <button className="flex flex-row items-center pl-5">
-          <div className="text-gray-6 pr-2">Next</div>
-          <IconArrowRight />
+          <div className="text-gray-6 pr-2 font-light">Next</div>
+          <IconArrowRight className="!w-[1.25rem] !h-[1.5rem]" />
         </button>
       </div>
-      <div className="text-gray-6 text-sm">{totalAgent} Agents in total</div>
+      <div className="text-gray-6 text-sm font-extralight">
+        {totalAgent} Agents in total
+      </div>
     </div>
   );
 };
