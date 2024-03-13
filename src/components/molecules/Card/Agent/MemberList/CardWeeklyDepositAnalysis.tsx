4309667ab5e9588f17593a2a62cecd9917cbfbb4
jordyandreas <jordyandreas76@gmail.com>
2024-03-13 13:13:33 +0700

    fix member list 1 Recent Markets - [x] - nilai "TotalBuy" = font-weight :300, letter spacing :0.2px - [x] - "TotalBuy:" dibuat jarak dengan "RP24.600.590,00" = 4px
    Weekly Deposit
    - [x] - area nilai deposit perhari dibuat "justify"
    - [x] - Warna area deposit perhari dibuat menjadi abu abu ketika tidak memiliki nilai
    - [x] - nilai Deposit perhari = font weight :300
    - [x] - Area Total Deposit week 1  = padding T B :1.188rem

--- src/components/molecules/Card/Agent/MemberList/CardWeeklyDepositAnalysis.tsx
+++ src/components/molecules/Card/Agent/MemberList/CardWeeklyDepositAnalysis.tsx
@@ -12,9 +12,9 @@ export const CardWeeklyDepositAnalysis: React.FC<ICardWeeklyDeposit> = ({
         {data.map((item: IWeeklyDeposit) => (
           <div
             key={`total-weekly-deposit-${item.id}`}
-            className="flex flex-row items-center bg-base-bg4 border border-base-bg3 rounded-xl py-[1.25rem] px-[1.188rem]"
+            className="flex flex-row items-center bg-base-bg4 border border-base-bg3 rounded-xl p-[1.188rem]"
           >
-            <div className="flex flex-col mr-[97px]">
+            <div className="flex w-[189px] flex-col mr-[97px]">
               <div className="flex flex-row items-center gap-[10px] mb-[6px]">
                 <div className="text-sm text-gray-6 font-extralight">
                   Total Deposit
@@ -30,21 +30,33 @@ export const CardWeeklyDepositAnalysis: React.FC<ICardWeeklyDeposit> = ({
               </div>
             </div>
             <div className="border-l border-l-gray-1 h-6" />
-            <div className="flex flex-row items-center ml-[46px] gap-2">
+            <div className="flex flex-1 flex-row items-center ml-[46px] gap-3">
               {item.weeklyDeposit.map((x: WeeklyDeposit) => (
                 <div
                   key={`weekly-deposit-${x.id}`}
-                  className="flex flex-row items-center min-w-[110px] bg-success-1 border border-success-2 rounded-full py-1 pl-1 pr-3 gap-2"
+                  className={`flex flex-row items-center w-[110px] border ${
+                    x.deposit === "0"
+                      ? "bg-base-bg3 border-base-bg3"
+                      : "bg-success-1 border-success-2"
+                  } rounded-full py-1 pl-1 pr-3 gap-2`}
                 >
-                  <div className="flex bg-success-1 border border-success-2 rounded-full h-6 w-6 justify-center items-center text-sm text-gray-10">
+                  <div
+                    className={`flex border rounded-full h-6 w-6 justify-center items-center text-sm ${
+                      x.deposit === "0"
+                        ? "text-gray-6 bg-base-bg3 border-base-bg3"
+                        : "text-gray-10 bg-success-1 border-success-2"
+                    }`}
+                  >
                     {x.codeDay}
                   </div>
                   <div
-                    className={`text-xs text-gray-10 ${
+                    className={`${
                       x.deposit === "0" && "flex flex-row items-center"
                     }`}
                   >
-                    Rp{x.deposit}
+                    <div className="text-xs text-gray-10 font-light">
+                      Rp{x.deposit}
+                    </div>
                     {x.deposit === "0" && (
                       <div className="text-xs text-gray-6">,00</div>
                     )}
