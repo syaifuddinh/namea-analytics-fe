dd0665f3f39026d160e87ca06b66e058b95af8f7
jordyandreas <jordyandreas76@gmail.com>
2024-03-14 14:32:50 +0700

    Data Analyst - Jarak Ethereums ke title  Data Analys di atas = Margin T :3.25rem - box dalam "2021", "2022", "2023" = Padding L R : 10px
    - area terluar Total Revenue = Padding :1.25rem
    - Area Total Revenue = Padding L R :1.125rem
    - Nilai Total Turnover = Warna dibuat sama dengan nilai Total Revenue
    
    - Lebar tiap box nilai perbulan = 87px
    - text "33M" = warna di buat lebih terang sesuai figma

--- src/components/atoms/dot-global/dot-global.tsx
+++ src/components/atoms/dot-global/dot-global.tsx
@@ -1,13 +1,18 @@
 import clsx from "clsx";
 import { IDotProps } from "./types";
 
-export const Dot: React.FC<IDotProps> = ({ className }) => {
+export const Dot: React.FC<IDotProps> = ({ className = "", profit }) => {
   return (
     <div
-      className={clsx(
-        "h-2 w-2 rounded-full border-[3px] mr-2 justify-center mt-1.5",
-        className
-      )}
-    />
+      className={`flex ${
+        profit ? "bg-dot-blue-border" : "bg-alert-2"
+      } h-3.5 w-3.5 rounded-full items-center justify-center ${className}`}
+    >
+      <div
+        className={`${
+          profit ? "bg-dot-blue" : "bg-alert-10"
+        } h-2 w-2 rounded-full`}
+      />
+    </div>
   );
 };
