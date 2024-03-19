dd36abf5fe521de80fa2a30e96971f77c0f4a273
Syaifuddin Hadi <syaifudinhadi8@gmail.com>
2024-03-19 15:37:20 +0700

    [EDIT] Costing / Promotional / Event : precising agent section

--- src/components/organisms/costing/agent-section/agent-card.tsx
+++ src/components/organisms/costing/agent-section/agent-card.tsx
@@ -17,8 +17,8 @@ export default function AgentCard({
 	revenue: string
 }) {
 	return (
-		<div className="agent-card bg-primary-gradient4 border border-gray-1 p-[3px] rounded-[18px]">
-			<div className="bg-base-bg5 h-[80px] rounded-[12px] overflow-hidden border border-base-bg3">
+		<div className="agent-card bg-primary-gradient4 border border-gray-1 pt-[2px] px-[2px] rounded-[18px]">
+			<div className="bg-base-bg5 h-[80px] rounded-[16px] overflow-hidden border border-base-bg3">
 				<div className="text-warning text-xs bg-bg-rank w-[50px] h-[20px] pl-[10px] pt-[2px] rounded-br-[6px]">
 					{ subtitle }
 				</div>
@@ -26,7 +26,7 @@ export default function AgentCard({
 				<div className="px-5 flex justify-between items-center">
 					<div className="flex items-center gap-4">
 						<div>{ icon }</div>
-						<div className="ml-[6px] font-light text-sm text-gray-10 leading-5">
+						<div className="font-light text-sm text-gray-10 leading-5">
 							{ title }
 						</div>
 					</div>
@@ -35,7 +35,7 @@ export default function AgentCard({
 				</div>
 			</div>
 
-			<div className="flex h-[92px] flex *:grow items-center justify-between">
+			<div className="flex h-[88px] flex *:grow items-center justify-between">
 				<div className="flex flex-col items-center">
 					<div className="font-extralight text-sm leading-5 text-gray-6">
 						Active Events
